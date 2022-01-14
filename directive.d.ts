b53c36abc6a1a49bc90538830eec90a1bb0a1aee
Wiens <Huangwenxu@yimian.com.cn>
2022-01-14 14:01:18 +0800

    mod: update .d.ts

--- directive.d.ts
+++ directive.d.ts
@@ -1,5 +1,3 @@
 declare module 'vue3-highlight-text' {
-  export function directive(
-    options: { mode: string = 'ig', color: string = '#EB9F13'}
-  ): (el: HTMLElement, binding: any) => void
+  export function directive(options = { mode: 'ig', color: '#EB9F13' }): (el: HTMLElement, binding: any) => void
 }
